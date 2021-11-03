47f7053ae98ac919af66aa87b69a42627cb6066b
Denis Zubarev <dvzubarev@yandex.ru>
2021-11-03 22:37:30 +0300

    add bindings for lang detection functions

--- src/exbase_bindings.cpp
+++ src/exbase_bindings.cpp
@@ -32,10 +32,26 @@ py::list detect_lang_calc_word_ids(const py::list& vs, bool recode2cp1251 = true
     return ids;
 }
 
+std::uint32_t lang_of_word_(const std::string& s){
+    return static_cast<std::uint32_t>(lang_of_word(s));
+}
+
+py::list lang_of_words(const py::list& strs){
+    py::list langs;
+
+    for(int i = 0; i < len(strs); ++i) {
+        py::str s = strs[i];
+        langs.append(lang_of_word_(s));
+    }
+    return langs;
+}
+
 PYBIND11_MODULE(libpyexbase, m) {
     m.def("calc_word_id", &calc_word_id_);
     m.def("detect_lang_calc_word_id", &detect_lang_calc_word_id);
     m.def("calc_word_ids", &calc_word_ids);
     m.def("detect_lang_calc_word_ids", &detect_lang_calc_word_ids);
     m.def("combine_word_id", &combine_word_id);
+    m.def("lang_of_word", &lang_of_word_);
+    m.def("lang_of_words", &lang_of_words);
 }
