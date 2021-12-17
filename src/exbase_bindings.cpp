dd9a6b6576da99f36f9a680c727817d1b48f9128
Denis Zubarev <dvzubarev@yandex.ru>
2021-12-17 14:11:31 +0300

    add new function

--- src/exbase_bindings.cpp
+++ src/exbase_bindings.cpp
@@ -1,6 +1,7 @@
 #include "str2word_id.hpp"
 
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 
 namespace py = pybind11;
 
@@ -32,6 +33,22 @@ py::list detect_lang_calc_word_ids(const py::list& vs, bool recode2cp1251 = true
     return ids;
 }
 
+std::tuple<std::vector<uint64_t>, std::vector<uint8_t>>
+calc_word_ids_and_langs(const py::list& vs, bool recode2cp1251 = true){
+
+    std::vector<uint64_t> ids(len(vs));
+    std::vector<uint8_t> langs(len(vs));
+
+    for(int i = 0; i < len(vs); ++i) {
+        py::str s = vs[i];
+        lang_id_t lang_id (lang_of_word(s));
+        langs[i] = static_cast<uint8_t>(lang_id);
+        ids[i] = calc_word_id(s, lang_id, recode2cp1251);
+    }
+
+    return std::tuple(std::move(ids), std::move(langs));
+}
+
 std::uint32_t lang_of_word_(const std::string& s){
     return static_cast<std::uint32_t>(lang_of_word(s));
 }
@@ -51,6 +68,7 @@ PYBIND11_MODULE(libpyexbase, m) {
     m.def("detect_lang_calc_word_id", &detect_lang_calc_word_id);
     m.def("calc_word_ids", &calc_word_ids);
     m.def("detect_lang_calc_word_ids", &detect_lang_calc_word_ids);
+    m.def("calc_word_ids_and_langs", &calc_word_ids_and_langs);
     m.def("combine_word_id", &combine_word_id);
     m.def("lang_of_word", &lang_of_word_);
     m.def("lang_of_words", &lang_of_words);
